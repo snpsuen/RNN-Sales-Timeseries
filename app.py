f4980c4a69bc28216d11e2387e9c99567f62f8db
snpsuen <66404197+snpsuen@users.noreply.github.com>
2022-05-07 14:37:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ def home():
 @app.route('/prediction', methods=['GET', 'POST'])
 def prediction():
   if request.method == 'POST':
-    month01 = float(request.form.get('month01'))
-    month02 = float(request.form.get('month02'))
-    month03 = float(request.form.get('month03'))
-    month04 = float(request.form.get('month04'))
-    month05 = float(request.form.get('month05'))	
- 
     n_steps = 5
-    row = asarray([month01, month02, month03, month04, month05]).reshape((1, n_steps, 1))
+	past=[]
+    for in range(1, 6)
+	  month = "month0" + str(i)
+	  result = float(request.form.get(month))
+	  past.append(result)
+  
+    row = asarray(past).reshape((1, n_steps, 1))
     yhat = model.predict(row)
-    return render_template('result.html', result=yhat)
+	return render_template('result.html', result=yhat)
